4c11ab7f9664080ca1b419087bdb914809b08359
sunmxt <sunmxt@126.com>
2018-03-31 14:33:22 +0800

    Add tests for session module

--- test/utils.py
+++ test/utils.py
@@ -6,7 +6,8 @@
 
 class HookedMethod:
     def __init__(self, _proc, _proc_arg):
-        def redirect_to(_hooked_self, *args, **kwargs):
-            _proc(_proc_arg, _hooked_self, *args, **kwargs)
-        self.__call__ = redirect_to     
+        self.__proc = _proc
+        self.__proc_arg = _proc_arg
 
+    def __call__(self, *args, **kwarg):
+        return self.__proc(self.__proc_arg, *args, **kwarg)
